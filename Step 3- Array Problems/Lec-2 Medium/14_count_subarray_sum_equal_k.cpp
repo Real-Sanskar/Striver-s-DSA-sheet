2d6111771f736d1330050da845ac1c5fd7740487
Real-Sanskar <sanskar18016@gmail.com>
2025-11-05 16:46:06 +0530

    Added dry run

--- Step 3- Array Problems/Lec-2 Medium/14_count_subarray_sum_equal_k.cpp
+++ Step 3- Array Problems/Lec-2 Medium/14_count_subarray_sum_equal_k.cpp
@@ -58,11 +58,32 @@ using namespace std;
 
 
 
+// 3. Optimal approach (prefix sum + hash map)
+
+// create map and store (0:1) [for sum == k]
+// Traverse the array 
+//      calculate prefixs sum
+//      calculate rem = sum - k
+//      update count
+//      update map
+
+
+
+// DRY RUN:
+// Input: {3, 1, 2, 4}, k = 6
+// sum = 3 → no match
+// sum = 4 → no match
+// sum = 6 → count += 1
+// sum = 10 → sum - k = 4 → count += 1
+// Final count = 2
+// Valid subarrays: {3,1,2}, {2,4}
+
+
 int count_sum(vector<int> &arr, int k){
     int n = arr.size();
     unordered_map<int,int> prefixSum;
 
-    prefixSum[0] = 1;
+    prefixSum[0] = 1;   // for sum == k condition
 
     int count = 0, sum = 0;
     for(int i=0; i<n; i++){
